464ae2b8cb1f646ea0c677110a3e49af780752ab
Thomas Scheier <Thomas.Scheier@gmail.com>
2013-01-06 19:50:24 -0600

    Initial attempt at First OpenGL program.

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@ if [[ ! -d .build ]]; then
     mkdir .build
 fi
 cd .build
-cmake -G Xcode ..
-make
+cmake -G "Xcode" ..
+xcodebuild -project HelloOpenGL.xcodeproj -alltargets
+
+#cmake -G "Unix Makefiles" ..
+#make
