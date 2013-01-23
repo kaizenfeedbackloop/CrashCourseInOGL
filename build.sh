a229a20e72971faa7b00025fa2598ff5910d0427
Thomas Scheier <Thomas.Scheier@gmail.com>
2013-01-22 23:08:58 -0600

    Updated with week 2's assignment.

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
+# Add cxxtest path to bin...
+PATH=~/Projects/cxxtest/bin:$PATH
+
 if [[ ! -d .build ]]; then
     mkdir .build
 fi
 cd .build
 cmake -G "Xcode" ..
-xcodebuild -project HelloOpenGL.xcodeproj -alltargets
+xcodebuild -project CrashCourseInOGL.xcodeproj -alltargets
 
 #cmake -G "Unix Makefiles" ..
 #make
