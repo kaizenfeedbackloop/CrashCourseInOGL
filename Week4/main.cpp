bc2791e8cabdf73cccf6f231b9bc6fd91fdaf53e
Thomas Scheier <tjscheier@gmail.com>
2013-02-06 06:46:54 +0000

    Added a color filter to isolate YUV components.

--- Week4/main.cpp
+++ Week4/main.cpp
@@ -24,6 +24,8 @@
 #include <math/matrix.hpp>
 #include <cmath>
 
+#include <iostream>
+
 const float pi = acos(-1.0f);
 
 #include "LoadShaders.h"
@@ -33,12 +35,25 @@ const float pi = acos(-1.0f);
 enum VAO_IDs { Triangles, NumVAOs };
 enum Buffer_IDs { ArrayBuffer, NumBuffers };
 enum RenderBuffer_IDs { Color, Depth, NumRenderBuffers };
+enum YuvComponent_IDs { AllComponents, YComponent, UComponent,
+						VComponent, NumYuvComponents };
 
 GLuint VAOs[NumVAOs];
 GLuint Buffers[NumBuffers];
 GLuint frameBuffer, renderBuffer[NumRenderBuffers];
+GLuint YuvBuffers[NumYuvComponents];
+
+const GLuint filterUBB = 0;
 
 const GLuint NumVertices = 5;
+
+void checkForError()
+{
+	GLenum error = glGetError();
+	if(error != GL_NO_ERROR) {
+		std::cerr << gluErrorString(error);
+	}
+}
 
 void initRenderBuffers()
 {
@@ -61,9 +76,70 @@ void initRenderBuffers()
 	glEnable(GL_DEPTH_TEST);
 }
 
+void initYuvTransforms()
+{
+	const mat4<GLfloat> rgbToYuv(0.299f, 0.587f, 0.114f, 0.0f,
+								 -0.14713f, -0.28886f, 0.436f, 0.0f,
+								 0.615f, -0.51499f, -0.10001f, 0.0f,
+								 0.0f, 0.0f, 0.0f, 1.0f);
+	
+	const mat4<GLfloat> yuvToRgb(1.0f, 0.0f, 1.13983f, 0.0f,
+								 1.0f, -0.39465f, -0.58060f, 0.0f,
+								 1.0f, 2.03211f, 0.0f, 0.0f,
+								 0.0f, 0.0f, 0.0f, 1.0f);
+
+	glGenBuffers(NumYuvComponents, YuvBuffers);
+
+	/* All... */
+	glBindBuffer(GL_UNIFORM_BUFFER, YuvBuffers[AllComponents]);
+	checkForError();
+	mat4<GLfloat> all(mat4<GLfloat>::Identity);
+	glBufferData(GL_UNIFORM_BUFFER, sizeof(mat4<GLfloat>), &all,
+		GL_STATIC_DRAW);
+	checkForError();
+
+	/* Y... */
+	glBindBuffer(GL_UNIFORM_BUFFER, YuvBuffers[YComponent]);
+	mat4<GLfloat> y =
+		yuvToRgb *
+		mat4<GLfloat>(1.0f, 0.0f, 0.0f, 0.0f,
+					 0.0f, 0.0f, 0.0f, 0.0f,
+					 0.0f, 0.0f, 0.0f, 0.0f,
+					 0.0f, 0.0f, 0.0f, 1.0f) *
+		rgbToYuv;
+	glBufferData(GL_UNIFORM_BUFFER, sizeof(mat4<GLfloat>), &y,
+		GL_STATIC_DRAW);
+
+	/* U... */
+	glBindBuffer(GL_UNIFORM_BUFFER, YuvBuffers[UComponent]);
+	mat4<GLfloat> u =
+		yuvToRgb *
+		mat4<GLfloat>(0.0f, 0.0f, 0.0f, 0.0f,
+					0.0f, 1.0f, 0.0f, 0.0f,
+					0.0f, 0.0f, 0.0f, 0.0f,
+					0.0f, 0.0f, 0.0f, 1.0f) *
+		rgbToYuv;
+	glBufferData(GL_UNIFORM_BUFFER, sizeof(mat4<GLfloat>), &u,
+		GL_STATIC_DRAW);
+
+	/* V... */
+	glBindBuffer(GL_UNIFORM_BUFFER, YuvBuffers[VComponent]);
+	mat4<GLfloat> v =
+		yuvToRgb *
+		mat4<GLfloat>(0.0f, 0.0f, 0.0f, 0.0f,
+					0.0f, 0.0f, 0.0f, 0.0f,
+					0.0f, 0.0f, 1.0f, 0.0f,
+					0.0f, 0.0f, 0.0f, 1.0f) *
+		rgbToYuv;
+	glBufferData(GL_UNIFORM_BUFFER, sizeof(mat4<GLfloat>), &v,
+		GL_STATIC_DRAW);
+}
+
 void init()
 {
 	initRenderBuffers();
+
+	initYuvTransforms();
 	
 	glGenVertexArrays(NumVAOs, VAOs);
 	glBindVertexArray(VAOs[Triangles]);
@@ -103,12 +179,15 @@ void main()\r\n\
 
 	GLchar const * fragmentSource =
 		"#version 410 core\r\n\
+layout (std140) uniform ColorConversion {\r\n\
+mat4 filter;\r\n\
+};\r\n\
 in vec4 diffuse;\r\n\
 out vec4 fColor;\r\n\
 \r\n\
 void main()\r\n\
 {\r\n\
-    fColor = diffuse;\r\n\
+    fColor = filter * diffuse;\r\n\
 }\r\n\
 ";
 
@@ -144,9 +223,14 @@ void main()\r\n\
 						  BUFFER_OFFSET(sizeof(vec4<GLfloat>))
 						  );
 	glEnableVertexAttribArray(colorLocation);
+
+	GLint filterUBI = glGetUniformBlockIndex(program, "ColorConversion");
+	checkForError();
+	glUniformBlockBinding(program, filterUBI, filterUBB);
+	checkForError();
 }
 
-void display()
+void drawToOffScreenFrameBuffer()
 {
 	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, frameBuffer);
 	glViewport(0, 0, 256, 256);
@@ -159,15 +243,43 @@ void display()
 
 	glFlush();
 
-	/* Present... */
+	/* Rest to Window Frame Buffer... */
 	glBindFramebuffer(GL_READ_FRAMEBUFFER, frameBuffer);
-	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0); /* Reset */
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+	glViewport(0,0,512,512);
+}
 
+void display()
+{
+	/* Clear... */
 	glViewport(0,0,512,512);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	glBindBufferRange(GL_UNIFORM_BUFFER, filterUBB, YuvBuffers[AllComponents],
+					  0, sizeof(mat4<GLfloat>));
+	checkForError();
+	drawToOffScreenFrameBuffer();
 	glBlitFramebuffer(0,0,255,255,0,256,255,511,
 		GL_COLOR_BUFFER_BIT, GL_NEAREST);
+
+	glBindBufferRange(GL_UNIFORM_BUFFER, filterUBB, YuvBuffers[YComponent],
+					  0, sizeof(mat4<GLfloat>));
+	drawToOffScreenFrameBuffer();
+	glBlitFramebuffer(0,0,255,255,0,0,255,255,
+		GL_COLOR_BUFFER_BIT, GL_NEAREST);
+
+	glBindBufferRange(GL_UNIFORM_BUFFER, filterUBB, YuvBuffers[UComponent],
+					  0, sizeof(mat4<GLfloat>));
+	drawToOffScreenFrameBuffer();
+	glBlitFramebuffer(0,0,255,255,256,256,511,511,
+		GL_COLOR_BUFFER_BIT, GL_NEAREST);
+
+	glBindBufferRange(GL_UNIFORM_BUFFER, filterUBB, YuvBuffers[VComponent],
+					  0, sizeof(mat4<GLfloat>));
+	drawToOffScreenFrameBuffer();
+	glBlitFramebuffer(0,0,255,255,256,0,511,255,
+		GL_COLOR_BUFFER_BIT, GL_NEAREST);
+
 	glutSwapBuffers();
 }
 
